b92787674c194360ba4f6d4a7487dd342f1b0c96
BT_1N3_05 <arafi6624@bths.edu>
2019-03-26 08:31:57 -0400

    Initial commit

---
+++ classes/IntArrayWorker.java
@@ -0,0 +1,139 @@
+import java.util.Scanner;
+
+public class IntArrayWorker
+{
+  /** two dimensional matrix */
+  private int[][] matrix = null;
+  
+  /** set the matrix to the passed one
+    * @param theMatrix the one to use
+    */
+  public void setMatrix(int[][] theMatrix)
+  {
+    matrix = theMatrix;
+  }
+  
+  /**
+   * Method to return the total 
+   * @return the total of the values in the array
+   */
+  public int getTotal()
+  {
+    int total = 0;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+        total = total + matrix[row][col];
+      }
+    }
+    return total;
+  }
+  
+  /**
+   * Method to return the total using a nested for-each loop
+   * @return the total of the values in the array
+   */
+  public int getTotalNested()
+  {
+    int total = 0;
+    for (int[] rowArray : matrix)
+    {
+      for (int item : rowArray)
+      {
+        total = total + item;
+      }
+    }
+    return total;
+  }
+  
+  /**
+   * Method to fill with an increasing count
+   */
+  public void fillCount()
+  {
+    int numCols = matrix[0].length;
+    int count = 1;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < numCols; col++)
+      {
+        matrix[row][col] = count;
+        count++;
+      }
+    }
+  }
+  
+  /**
+   * print the values in the array in rows and columns
+   */
+  public void print()
+  {
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+        System.out.print( matrix[row][col] + " " );
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
+  
+  
+  /** 
+   * fill the array with a pattern
+   */
+  public void fillPattern1()
+  {
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; 
+           col++)
+      {
+        if (row < col)
+          matrix[row][col] = 1;
+        else if (row == col)
+          matrix[row][col] = 2;
+        else
+          matrix[row][col] = 3;
+      }
+    }
+  }
+
+  public void getCount()
+  {
+    int m, n, c, d, nr = 0, x;
+
+    java.util.Scanner in = new Scanner(System.in);
+
+    System.out.println("Enter the number of rows:");
+    m = in.nextInt();
+    System.out.println("Enter the number of columns:");
+    n  = in.nextInt();
+
+    int matrix[][] = new int[m][n];
+
+    System.out.println("Enter the elements of the matrix:");
+
+    for (  c = 0 ; c < m ; c++ )
+      for ( d = 0 ; d < n ; d++ )
+        matrix[c][d] = in.nextInt();
+
+    System.out.println("Enter the value for number x:");
+    x = in.nextInt();
+
+    for (  c = 0 ; c < m ; c++ ) {
+      if(c == x)
+        nr++;
+    }
+
+    for ( d = 0 ; d < n ; d++ ) {
+      if(d == x)
+        nr++;
+    }
+
+    System.out.println("Number x appears " + nr + " times.");
+  }
+ 
+}
